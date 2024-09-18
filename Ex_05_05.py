e603d329d7f837ba505fd9a907442507ee1cf25e
kidstonek <96789294+kidstonek@users.noreply.github.com>
2024-09-18 08:37:03 +0300

    Ex 5 and 6 done?

--- Ex_05_05.py
+++ Ex_05_05.py
@@ -1,7 +1,50 @@
 """
 Завдання 5
 
-Використовуючи код завдання 2 надрукуйте у терміналі інформацію, яка міститься у класах Contact
-та UpdateContact та їх екземплярах. Видаліть атрибут job, і знову надрукуйте стан класів та їх
+Використовуючи код завдання 2 надрукуйте у терміналі інформацію,
+яка міститься у класах Contact та UpdateContact та їх екземплярах.
+Видаліть атрибут job, і знову надрукуйте стан класів та їх
 екземплярів. Порівняйте їх. Зробіть відповідні висновки.
 """
+
+
+class Contact:
+    def __init__(self, surname: str, name: str, age: int, mob_phone: str, email: str) -> None:
+        self.surname = surname
+        self.name = name
+        self.age = age
+        self.mob_phone = mob_phone
+        self.email = email
+
+    def get_contact(self):
+        return f'{self.name} has mobile {self.mob_phone} and email {self.email}'
+
+    def sent_message(self):
+        return f'the message was sent trough email {self.email}'
+
+
+class UpdateContact(Contact):
+    def __init__(self, surname: str, name: str, age: int, mob_phone: str, email: str, job) -> None:
+        super().__init__(surname, name, age, mob_phone, email)
+        self.job = job
+
+    def get_message(self):
+        return f'messages from the number {self.mob_phone}'
+
+
+if __name__ == '__main__':
+    print(dir(Contact))
+    print(dir(UpdateContact))
+    contact1 = Contact('Petrosky', 'Fedir', 40, '+380971233243', 'pet.fed@gmail.com')
+    upd_contact1 = UpdateContact('Fedoriv', 'Ilya', 24, '+380974562312', 'ilyafed@gmail.com', 'lawyer')
+
+    print(' __dict__')
+    print(f'for Contact - {contact1.__dict__}')
+    print(f'for UpdateContact - {upd_contact1.__dict__}')
+
+    print(f'was {upd_contact1.__dir__()}')
+    delattr(upd_contact1, 'job')
+    print(f'become {upd_contact1.__dir__()}')
+
+    print(dir(Contact))
+    print(dir(UpdateContact))
--- Ex_05_06.py
+++ Ex_05_06.py
@@ -1,6 +1,44 @@
 """
 Завдання 6
 
-Використовуючи код завдання 2 надрукуйте у терміналі всі методи, які містяться у класі Contact та UpdateContact.
+Використовуючи код завдання 2 надрукуйте у терміналі всі методи,
+які містяться у класі Contact та UpdateContact.
 
 """
+
+class Contact:
+    def __init__(self, surname: str, name: str, age: int, mob_phone: str, email: str) -> None:
+        self.surname = surname
+        self.name = name
+        self.age = age
+        self.mob_phone = mob_phone
+        self.email = email
+
+    def get_contact(self):
+        return f'{self.name} has mobile {self.mob_phone} and email {self.email}'
+
+    def sent_message(self):
+        return f'the message was sent trough email {self.email}'
+
+
+class UpdateContact(Contact):
+    def __init__(self, surname: str, name: str, age: int, mob_phone: str, email: str, job) -> None:
+        super().__init__(surname, name, age, mob_phone, email)
+        self.job = job
+
+    def get_message(self):
+        return f'messages from the number {self.mob_phone}'
+
+
+if __name__ == '__main__':
+    contact1 = Contact('Petrosky', 'Fedir', 40, '+380971233243', 'pet.fed@gmail.com')
+    upd_contact1 = UpdateContact('Fedoriv', 'Ilya', 24, '+380974562312', 'ilyafed@gmail.com', 'lawyer')
+
+    print('methods for Contact:')
+    for method in dir(Contact):
+        print(method)
+
+
+    print('\nmethods for UpdateContact:')
+    for method in dir(UpdateContact):
+        print(method)
